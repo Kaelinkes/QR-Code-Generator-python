18ca878eaac9ec96ec3d654390955d54d668be36
EXO_Kaelin <134412829+Kaelinkes@users.noreply.github.com>
2026-03-13 13:37:45 +0200

    Add files via upload

--- Project_file/QR_Code_genrator.py
+++ Project_file/QR_Code_genrator.py
@@ -1,1 +1,35 @@
+import qrcode
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
+def qrcode_genrator(URL): 
+    
+    URL = URL.strip()
+
+    qr = qrcode.QRCode()
+    qr.add_data(URL)
+
+    img = qr.make_image()
+    img.save("qrcode.png")
+def display_qrcode():
+    
+    img = mpimg.imread("qrcode.png")
+
+    plt.imshow(img,cmap='grey')
+    plt.axis('off')  
+    plt.show()  
+
+print("=============================")
+print("        QR CODE MAKER        ")
+print("=============================")
+print("")
+print("Welcome to my qrcode genrator")
+
+url = input("Enter the URL: ")
+qrcode_genrator(url)
+Yes = input("Display QR code? (Y/n): ")
+if Yes.lower() == "y":
+    display_qrcode() 
+else:
+    print("Invalid pick!")
+print("Exiting...")
